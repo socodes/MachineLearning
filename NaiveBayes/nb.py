65c7e96ad32b9243405a7c3d72fc3bbac1e7657a
Muhammed Didin <mdidin80@gmail.com>
2020-04-14 12:27:37 +0300

    Add files via upload

---
+++ NaiveBayes/nb.py
@@ -0,0 +1,27 @@
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler 
+
+veriler = pd.read_csv('dataset/veriler.csv')
+x = veriler.iloc[:,1:4].values
+y = veriler.iloc[:,4:].values
+
+
+sc= StandardScaler()
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.33,random_state=0)
+X_train=sc.fit_transform(x_train)
+X_test=sc.transform(x_test)
+
+from sklearn.naive_bayes import GaussianNB
+
+gnb = GaussianNB()
+gnb.fit(X_train,y_train)
+
+y_pred = gnb.predict(X_test)
+
+cm= confusion_matrix(y_test,y_pred)
+print('GNB')
+print(cm)
